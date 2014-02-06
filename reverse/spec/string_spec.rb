8869123034ba5ed2370449e11370ef785a31260e
Peter <peterhsaxton@gmail.com>
2014-02-06 11:57:37 +0000

    Added alternatives to reverse string exercise

--- reverse/spec/string_spec.rb
+++ reverse/spec/string_spec.rb
@@ -5,4 +5,16 @@ describe 'string reverse function' do
   it 'should reverse a simple string' do
     expect(simple.reverse0).to eq('fedcba')
   end
+  
+  it 'should reverse a simple string' do
+    expect(simple.reverse1).to eq('fedcba')
+  end
+  
+  it 'should reverse a simple string' do
+    expect(simple.reverse2).to eq('fedcba')
+  end
+  
+  it 'should reverse a simple string' do
+    expect(simple.reverse3).to eq('fedcba')
+  end
 end
--- reverse/string.rb
+++ reverse/string.rb
@@ -7,4 +7,23 @@ class String
     end
     output
   end
+  
+  def reverse1 output = []
+    for i in (1..length)
+      output << self[-1*i]
+    end
+    output.join
+  end
+  
+  def reverse2
+    array = split("")
+    while l = array.pop
+     result = result ? result + l : l
+    end
+    result
+  end
+  
+  def reverse3
+    chars.inject{ |mem, char| char + mem }
+  end
 end
